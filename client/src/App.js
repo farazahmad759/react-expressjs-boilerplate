ea18f76e06d967335838cb26a15f10d733fe5124
Faraz Ahmad <farazahmad759@gmail.com>
2020-12-02 18:23:20 +0500

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+function App() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/users").then((res) => {
+      setData(res.data);
+    });
+  });
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
